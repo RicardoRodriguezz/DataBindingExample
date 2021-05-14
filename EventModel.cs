e64dc39ae77710e683774f1c18f043740e1b4769
RicardoRodriguezZapata <17470068@campeche.tecnm.mx>
2021-05-14 00:52:51 -0500

    Version utilizando Data Binding a un obejto de datos simple.

---
+++ EventModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DataBindingExample
+{
+    class EventModel : INotifyPropertyChanged
+    {
+        private string title;
+        private DateTime date;
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (value != title)
+                {
+                    title = value;
+                    OnPropertyChanged("Title");
+                }
+            }
+        }
+        public DateTime Date
+        {
+            get => date;
+            set
+            {
+                if (value != date)
+                {
+                    date = value;
+                    OnPropertyChanged("Date");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
